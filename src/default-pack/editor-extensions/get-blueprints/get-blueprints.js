a85011a568d949a19999fe43197a829804cad1ca
benhar-dev <53424421+benhar-dev@users.noreply.github.com>
2025-01-31 17:32:22 +1100

    updated text

--- src/default-pack/editor-extensions/get-blueprints/get-blueprints.js
+++ src/default-pack/editor-extensions/get-blueprints/get-blueprints.js
@@ -24,7 +24,7 @@ export class GetBlueprintsExtension {
     LiteGraph.log_log("api get blueprints");
     this.editor.showInfo(
       "Loading Blueprints",
-      "`Please wait` while we fetch the blueprints from the server."
+      "Please wait while we fetch the blueprints from the server."
     );
     this.getBlueprintsButton.disable();
     try {
